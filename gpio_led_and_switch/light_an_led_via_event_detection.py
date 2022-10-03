b539a1204c3c23ebb18078244567c30129644553
Daniel Walker <dnwlker@gmail.com>
2022-10-03 00:06:06 -0700

    adding the ability to count LED lighting events with debounce logic

--- gpio_led_and_switch/light_an_led_via_event_detection.py
+++ gpio_led_and_switch/light_an_led_via_event_detection.py
@@ -11,13 +11,34 @@ GPIO.output(ledPin, ledState)
 
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
+counter = 0
+lastTime = 0.
+lastState = None
+shortestCountableInterval = 0.15
 def respondToButtonTransition(channel):
-        if GPIO.input(buttonPin) == GPIO.LOW:
-            ledState = GPIO.HIGH
-        else:
-            ledState = GPIO.LOW
-        GPIO.output(ledPin, ledState)
+    global counter
+    global lastTime
+    global lastState
+    thisTime = time.time()
+    if GPIO.input(buttonPin) == GPIO.LOW:
+        ledState = GPIO.HIGH
+    else:
+        ledState = GPIO.LOW
+    GPIO.output(ledPin, ledState)
+
+    # first time - depending on where in bounce, could be high or low
+    if lastState is None:
+        if ledState:
+            lastTime = thisTime
+            counter += 1
+            print(counter)
+    elif ledState != lastState:
+        if thisTime > lastTime + shortestCountableInterval:
+            lastTime = thisTime
+            if ledState:
+                counter += 1
+                print(counter)
+    lastState = ledState
 
 GPIO.add_event_detect(buttonPin, GPIO.BOTH, callback=respondToButtonTransition)
 
