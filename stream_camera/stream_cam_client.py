b8ed931b86d9b46dace5604eac6a76ea31ff10d1
Daniel Walker <dnwlker@gmail.com>
2022-10-09 00:12:23 -0700

    a few more options on client side for cam streaming

--- stream_camera/stream_cam_client.py
+++ stream_camera/stream_cam_client.py
@@ -7,13 +7,14 @@ import picamera
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket()
-client_socket.connect(('my_server', 8000))
+client_socket.connect(('Walker', 8000))
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
 try:
     camera = picamera.PiCamera()
     camera.resolution = (640, 480)
+    camera.framerate = 5
     # Start a preview and let the camera warm up for 2 seconds
     camera.start_preview()
     time.sleep(2)
