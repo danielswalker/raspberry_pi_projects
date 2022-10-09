a56b233bf785e042e4758ced9195067b68da8cf9
Daniel Walker <dnwlker@gmail.com>
2022-10-09 00:10:25 -0700

    cleaning up server for keystroke forwarding

--- grpc_forward_keypresses/server.py
+++ grpc_forward_keypresses/server.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 import grpc
 from concurrent import futures
-from monitor_keypress_pb2 import KeysPressed, Empty
-import monitor_keypress_pb2_grpc
+from send_keypresses_pb2 import KeysPressed, Empty
+import send_keypresses_pb2_grpc
 
-class MonitorKeyboardService(
-            monitor_keypress_pb2_grpc.KeyboardMonitoringServicer):
-    def HandleButtonEvent(self, request, context):
-        print(request.KeysPressed)
-        return Empty
+class KeySenderService(
+            send_keypresses_pb2_grpc.KeySenderServicer):
+    def HandleKeyEvent(self, request, context):
+        print(request.keys)
+        return Empty()
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    monitor_keyboard_pb2_grpc.add_KeyboardMonitoringServicer_to_server(
-        MonitorKeyboardService(), server)
+    send_keypresses_pb2_grpc.add_KeySenderServicer_to_server(
+        KeySenderService(), server)
     server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
