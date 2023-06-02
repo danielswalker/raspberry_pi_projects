4663e0d389d5634836faec046bbae6f39ca9b421
Daniel Walker <dnwlker@gmail.com>
2023-06-02 00:43:18 -0700

    this version of am2302.c seems to be working repeatably, not sure what timing issue was tripping me up before

--- am2302/am2302.c
+++ am2302/am2302.c
@@ -35,6 +35,7 @@ int main() {
     printf("mmap error %p\n", gpio_map);
     exit(-1);
   }
+  printf("Mem mapped\n");
 
   // Always use volatile pointer!
   gpio = (volatile unsigned *)gpio_map;
@@ -46,19 +47,24 @@ int main() {
   usleep(1);
   *(gpio + 37) = 0;
   *(gpio + 38) = 0;
+  printf("Set pull up\n");
 
   // Set high by writing bit 19 in output set register 0
   *(gpio + 7) = 1 << 19;  // Set GPIO19 high (zeros have no effect)
+  printf("Set high\n");
 
   // Each function select register controls 10 pins. Each pin has 3 bits to
   // control, and we want to ensure we set pin 19's bit to 001.
   *(gpio + 1) &= ~(7 << ((19 % 10) * 3));  // clear bits
   *(gpio + 1) |= 1 << ((19 % 10) * 3);     // set as output
+  printf("Set to output\n");
   usleep(5000);
 
   // Set low by writing bit 19 in output clear register 0
   *(gpio + 10) = 1 << 19;  // Set GPIO19 low (zeros have no effect)
+  printf("Set low\n");
   usleep(5000);
+  printf("About to set high and go to input\n");
   *(gpio + 7) = 1 << 19;  // Set GPIO19 high (zeros have no effect)
   // Clear GPIO19 bits in Function Select Register to set as input
   *(gpio + 1) &= ~(7 << ((19 % 10) * 3));
@@ -68,11 +74,12 @@ int main() {
   *(gpio + 22) |= (1 << 19);
 
   // Start polling
-  struct timespec delay = {0, 10000};
+  struct timespec delay = {0, 2000};
   struct timespec edgeTimes[42];
-  int idx;
+  int idx = 0;
   struct timespec start;
   clock_gettime(CLOCK_MONOTONIC, &start);
+  printf("About to start polling\n");
   while (1) {
     if (*(gpio + 16) & (1 << 19)) {  // If event detected
       // Clear event
@@ -87,12 +94,13 @@ int main() {
 
     struct timespec now;
     clock_gettime(CLOCK_MONOTONIC, &now);
-    printf("%lld.%.9ld\n", (long long)now.tv_sec, now.tv_nsec);
+    // printf("%lld.%.9ld\n", (long long)now.tv_sec, now.tv_nsec);
     if ((now.tv_sec - start.tv_sec) > 2) {
       printf("Timed out!\n");
       break;
     }
   }
+  printf("Done polling\n");
 
   // turn off edge detection
   *(gpio + 22) = 0;
@@ -104,23 +112,24 @@ int main() {
     // return 1;
   }
 
-  printf("Timestamps:\n");
-  for (int i = 0; i < idx; i++) {
-    printf("%lld.%.9ld\n", (long long)edgeTimes[i].tv_sec,
-           edgeTimes[i].tv_nsec);
-  }
-
-  // for (int i = 1; i < idx; i++) {
-  //   long long seconds =
-  //       (long long)(edgeTimes[i].tv_sec - edgeTimes[i - 1].tv_sec);
-  //   long nanoseconds = edgeTimes[i].tv_nsec - edgeTimes[i - 1].tv_nsec;
-
-  //   if (nanoseconds < 0) {
-  //     seconds--;
-  //     nanoseconds += 1000000000;
-  //   }
-
-  //   printf("%lld.%.9ld\n", seconds, nanoseconds);
+  // printf("Timestamps:\n");
+  // for (int i = 0; i < idx; i++) {
+  //   printf("%lld.%.9ld\n", (long long)edgeTimes[i].tv_sec,
+  //          edgeTimes[i].tv_nsec);
   // }
+
+  for (int j = 1; j < idx; j++) {
+    long long seconds =
+        (long long)(edgeTimes[j].tv_sec - edgeTimes[j - 1].tv_sec);
+    long nanoseconds = edgeTimes[j].tv_nsec - edgeTimes[j - 1].tv_nsec;
+
+    if (nanoseconds < 0) {
+      seconds--;
+      nanoseconds += 1000000000;
+    }
+
+    printf("%ld\n", nanoseconds / 1000);
+    // printf("%d\n", j);
+  }
   return 0;
 }
